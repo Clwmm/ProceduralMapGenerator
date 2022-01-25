4b34f195b3b29a0fc7684234ca65022f3c76a5d7
Clwm <emgie4432@gmail.com>
2022-01-25 23:54:22 +0100

    Better Algorithm

--- Procedural Map Generator/MapGenerator.cpp
+++ Procedural Map Generator/MapGenerator.cpp
@@ -43,53 +43,56 @@ MapGenerator::MapGenerator()
 			rooms.push_back(a);
 			break;
 		default:
-			randomBefore = rand() % i;
-
-			position_before = rooms[randomBefore]->getPosition();
-			sizeBefore = rooms[randomBefore]->getSize();
-			
-			a->setFillColor(sf::Color(fromRandom(0, 255), fromRandom(0, 255), fromRandom(0, 255)));
-			a->setPosition(position_before);
-
-			chose = rand() % 4;
-			switch (chose)
+			bool generated = false;
+			while (!generated)
 			{
-			case 0: // gernerate on TOP of the previous one
-				moveX = fromRandom((-0.5 * sizeBefore.x) - (0.5 * randWidthRoom) + moveRandomRoom, (0.5 * sizeBefore.x) + (0.5 * randWidthRoom) - moveRandomRoom);
-				moveY = -0.5 * (static_cast<long long>(sizeBefore.y) + static_cast<long long>(randHeightRoom)) - distanceBetween;
-				break;
-			case 1: // LEFT
-				moveX = -0.5 * (static_cast<long long>(sizeBefore.x) + static_cast<long long>(randWidthRoom)) - distanceBetween;
-				moveY = fromRandom((-0.5 * sizeBefore.y) - (0.5 * randHeightRoom) + moveRandomRoom, (0.5 * sizeBefore.y) + (0.5 * randHeightRoom) - moveRandomRoom);
-				break;
-			case 2: // BOTTOM
-				moveX = fromRandom((-0.5 * sizeBefore.x) - (0.5 * randWidthRoom) + moveRandomRoom, (0.5 * sizeBefore.x) + (0.5 * randWidthRoom) - moveRandomRoom);
-				moveY = 0.5 * (static_cast<long long>(sizeBefore.y) + static_cast<long long>(randHeightRoom)) + distanceBetween;
-				break;
-			case 3: // RIGHT
-				moveX = 0.5 * (static_cast<long long>(sizeBefore.x) + static_cast<long long>(randWidthRoom)) + distanceBetween;
-				moveY = fromRandom((-0.5 * sizeBefore.y) - (0.5 * randHeightRoom) + moveRandomRoom, (0.5 * sizeBefore.y) + (0.5 * randHeightRoom) - moveRandomRoom);
-				break;
-			}
-			a->move(moveX, moveY);
-			
-			thisHalfSize = a->getSize() / 2.0f;
-			thisPosition = a->getPosition();
-			bool colision = false;
-			for (auto c : rooms)
-			{
-				beforeHalfSize = c->getSize() / 2.0f;
-				beforePosition = c->getPosition();
-				if (checkCollision(beforePosition, beforeHalfSize, thisPosition, sf::Vector2f(thisHalfSize.x + distanceBetween, thisHalfSize.y + distanceBetween)))
+				randomBefore = rand() % i;
+
+				position_before = rooms[randomBefore]->getPosition();
+				sizeBefore = rooms[randomBefore]->getSize();
+
+				a->setFillColor(sf::Color(fromRandom(0, 255), fromRandom(0, 255), fromRandom(0, 255)));
+				a->setPosition(position_before);
+
+				chose = rand() % 4;
+				switch (chose)
 				{
-					i--;
-					colision = true;
-					delete a;
+				case 0: // gernerate on TOP of the previous one
+					moveX = fromRandom((-0.5 * sizeBefore.x) - (0.5 * randWidthRoom) + moveRandomRoom, (0.5 * sizeBefore.x) + (0.5 * randWidthRoom) - moveRandomRoom);
+					moveY = -0.5 * (static_cast<long long>(sizeBefore.y) + static_cast<long long>(randHeightRoom)) - distanceBetween;
+					break;
+				case 1: // LEFT
+					moveX = -0.5 * (static_cast<long long>(sizeBefore.x) + static_cast<long long>(randWidthRoom)) - distanceBetween;
+					moveY = fromRandom((-0.5 * sizeBefore.y) - (0.5 * randHeightRoom) + moveRandomRoom, (0.5 * sizeBefore.y) + (0.5 * randHeightRoom) - moveRandomRoom);
+					break;
+				case 2: // BOTTOM
+					moveX = fromRandom((-0.5 * sizeBefore.x) - (0.5 * randWidthRoom) + moveRandomRoom, (0.5 * sizeBefore.x) + (0.5 * randWidthRoom) - moveRandomRoom);
+					moveY = 0.5 * (static_cast<long long>(sizeBefore.y) + static_cast<long long>(randHeightRoom)) + distanceBetween;
+					break;
+				case 3: // RIGHT
+					moveX = 0.5 * (static_cast<long long>(sizeBefore.x) + static_cast<long long>(randWidthRoom)) + distanceBetween;
+					moveY = fromRandom((-0.5 * sizeBefore.y) - (0.5 * randHeightRoom) + moveRandomRoom, (0.5 * sizeBefore.y) + (0.5 * randHeightRoom) - moveRandomRoom);
 					break;
 				}
+				a->move(moveX, moveY);
+
+				thisHalfSize = a->getSize() / 2.0f;
+				thisPosition = a->getPosition();
+				bool colision = false;
+				for (auto c : rooms)
+				{
+					beforeHalfSize = c->getSize() / 2.0f;
+					beforePosition = c->getPosition();
+					if (checkCollision(beforePosition, beforeHalfSize, thisPosition, sf::Vector2f(thisHalfSize.x + distanceBetween, thisHalfSize.y + distanceBetween)))
+					{
+						colision = true;
+						break;
+					}
+				}
+				if (!colision)
+					generated = true;
 			}
-			if (colision)
-				break;
+			
 			rooms.push_back(a);
 		}
 	}
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ MapGenerator::roomSizeTo
 MapGenerator::distanceBetween
 - You can change the number of rooms:  
 MapGenerator::noOfRooms //Min 1  
-With the i5-9400f, it takes about 17 seconds to generate 10,000 rooms.
+With the i5-9400f, it takes about 15 seconds to generate 10,000 rooms.
 - Always set the MapGenerator::moveRandomRoom value less than or egual to MapGenerator::roomSizeFrom
 
 
